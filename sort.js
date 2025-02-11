4c1edb5588f96382e46c565559ef02d408e5c0d5
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-11 16:50:41 +0600

    reading more

---
+++ sort.js
@@ -0,0 +1,3 @@
+const person = ['alim', 'sakib', 'rafiq', 'karim', 'rahim', 'jamal', 'kamal', 'salam', 'kalam'];
+const soortePerson = person.sort();
+console.log(soortePerson); // [ 'alim', 'jamal', 'kalam', 'karim', 'kamal', 'rahim', 'rafiq', 'sakib', 'salam' ]
